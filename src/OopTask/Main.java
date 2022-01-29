7d39173ba9604d3ff730f8a9bb6a80b3cbf4ab4f
Andrey0201 <andriutsa1@ukr.net>
2022-01-30 00:10:13 +0200

    add get and set

--- src/OopTask/Main.java
+++ src/OopTask/Main.java
@@ -8,14 +8,25 @@ public class Main {
         Box box4 = new Box(4, 5, 6);
         Box box5 = new Box(6, 7, 8);
 
-
-
         System.out.printf("box1 volume =%4d cm³\n", box1.volume());
         System.out.printf("box2 volume =%4d cm³\n", box2.volume());
         System.out.printf("box3 volume =%4d cm³\n", box3.volume());
         System.out.printf("box4 volume =%4d cm³\n", box4.volume());
         System.out.printf("box5 volume =%4d cm³\n", box5.volume());
+        System.out.println();
 
+        box1.setWidth(2);
+        box1.setHeight(2);
+        box1.setLength(2);
+
+        System.out.printf("box1 volume =%4d cm³\n", box1.volume());
+
+        System.out.println();
+        System.out.printf("""
+                width box2 =%2d cm
+                height box2 =%2d cm
+                length box2 =%2d cm
+                \s""",box2.getWidth(),box2.getHeight(),box2.getLength());
 
     }
 
